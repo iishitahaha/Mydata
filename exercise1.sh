f3331c8f86f41194f8907a91d10f7d2ec7fe2bed
Ishita <ishitamevada1931@gmail.com>
2022-06-15 16:42:12 +0530

    Second commit

--- exercise1.sh
+++ exercise1.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "Shell Scripting is Fun!"
+
+compliment="Shell Scripting is Fun!"
+host=$(hostname)
+
+echo $compliment
+
+echo "The script is running on $host, where $host is the output of the 'hostname'."
+
+if [-e /etc/shadow]
+then
+	echo "Shadow passwords are enabled."
+	echo "You have permission to edit /etc/shadow."
+else
+	echo "You do NOT have permission to edit /etc/shadow."
+fi
